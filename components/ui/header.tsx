13c7cb4056445f61420eed90e2fd3fa2d4282a3c
rosanno <rosannomacabuhay@gmail.com>
2024-04-08 13:25:15 +0800

    add header component

---
+++ components/ui/header.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Plus } from "lucide-react";
+
+import { Button } from "./button";
+
+const Header = ({
+  title,
+  button = false,
+}: {
+  title: string;
+  button?: boolean;
+}) => {
+  const router = useRouter();
+
+  return (
+    <header className="space-y-10">
+      <div className="flex items-center justify-between">
+        <h1 className="font-semibold text-lg tracking-wide">
+          {title}
+        </h1>
+        {button && (
+          <Button
+            size={"sm"}
+            className="text-[13px]"
+            onClick={() =>
+              router.push("/manage-restaurant/create")
+            }
+          >
+            <Plus className="mr-1 h-4 w-4" />
+            <span className="pt-0.5">Add Restaurant</span>
+          </Button>
+        )}
+      </div>
+      <div className="border-b border-b-gray-100/80" />
+    </header>
+  );
+};
+
+export default Header;
