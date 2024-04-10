fff91f7e6a51e1c88b10343b52bcb2d30c984147
rosanno <rosannomacabuhay@gmail.com>
2024-04-10 22:37:45 +0800

    removed divider

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -2,9 +2,8 @@
 
 const Header = ({ title }: { title: string }) => {
   return (
-    <header className="space-y-10">
+    <header className="space-y-10 pb-5">
       <h1 className="text-lg tracking-wide">{title}</h1>
-      <div className="border-b border-b-gray-100/80" />
     </header>
   );
 };
