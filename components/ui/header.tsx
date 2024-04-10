f48a2d5ea7901b8b8269846c4a26407380cc2190
rosanno <rosannomacabuhay@gmail.com>
2024-04-10 22:20:10 +0800

    removed useRouter

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-
 const Header = ({ title }: { title: string }) => {
-  const router = useRouter();
-
   return (
     <header className="space-y-10">
       <h1 className="text-lg tracking-wide">{title}</h1>
