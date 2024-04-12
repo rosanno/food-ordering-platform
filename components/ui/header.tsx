ad1617afb99227ea68e05705c28741f7716ddfc1
rosanno <rosannomacabuhay@gmail.com>
2024-04-12 17:16:46 +0800

    added breadcrumbs

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import CommonBreadcrumbs from "./common-breadcrumbs";
+
 const Header = ({ title }: { title: string }) => {
   return (
     <header className="space-y-10 pb-5">
-      <h1 className="text-lg tracking-wide">{title}</h1>
+      <div className="flex items-center gap-7">
+        <h1 className="text-lg tracking-wide">{title}</h1>
+        <CommonBreadcrumbs />
+      </div>
     </header>
   );
 };
