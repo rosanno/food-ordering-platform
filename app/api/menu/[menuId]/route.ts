cbecf0a1963818344fc1bd870d1789a9925b7963
rosanno <rosannomacabuhay@gmail.com>
2024-04-13 20:03:01 +0800

    created delete menu route handler

--- app/api/menu/[menuId]/route.ts
+++ app/api/menu/[menuId]/route.ts
@@ -40,3 +40,35 @@ export async function PATCH(
     });
   }
 }
+
+export async function DELETE(
+  request: NextRequest,
+  {
+    params,
+  }: {
+    params: { menuId: string };
+  }
+) {
+  try {
+    if (!params.menuId) {
+      return new NextResponse("Invalid menu id", {
+        status: 400,
+      });
+    }
+
+    await prisma.menu.delete({
+      where: {
+        id: params.menuId,
+      },
+    });
+
+    return NextResponse.json({
+      message: "Menu deleted",
+    });
+  } catch (error) {
+    console.log("[DELETE_PATCH]", error);
+    return new NextResponse("Internal error", {
+      status: 500,
+    });
+  }
+}
