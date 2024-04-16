675cdaf3efbb1b6764cc5336caa56aaa8d673bfc
rosanno <rosannomacabuhay@gmail.com>
2024-04-16 16:10:20 +0800

    modified checkRole and redirect on admin pages

--- app/admin/(routes)/dashboard/page.tsx
+++ app/admin/(routes)/dashboard/page.tsx
@@ -1,12 +1,3 @@
-import { redirect } from "next/navigation";
-
-import { checkRole } from "@/utils/roles";
-
 export default function AdminDashboard() {
-  // If the user does not have the admin role, redirect them to the home page
-  if (!checkRole("admin")) {
-    redirect("/");
-  }
-
   return <div></div>;
 }
--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,10 +1,17 @@
 import DashboardNavbar from "@/components/admin-navbar";
+import { checkRole } from "@/utils/roles";
+import { redirect } from "next/navigation";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // If the user does not have the admin role, redirect them to the home page
+  if (!checkRole("admin")) {
+    redirect("/");
+  }
+
   return (
     <>
       <DashboardNavbar />
