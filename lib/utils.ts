3abb1c89ece3e06b2258ebf9b2d56aebdf5f3834
rosanno <rosannomacabuhay@gmail.com>
2024-04-16 20:33:36 +0800

    created currency formatter helper

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatCurrency = (
+  value: number,
+  currency: string = "USD"
+): string => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency,
+  }).format(value);
+};
