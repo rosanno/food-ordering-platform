6b595400ed1d47a26cd7dc672f293c7781a087e3
rosanno <rosannomacabuhay@gmail.com>
2024-04-16 11:38:12 +0800

    update auth middleware

---
+++ app/(site)/(routes)/menu/page.tsx
@@ -0,0 +1,5 @@
+const MenuPage = () => {
+  return <div>Menu Page</div>;
+};
+
+export default MenuPage;
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,28 @@
-import { authMiddleware } from "@clerk/nextjs";
+import {
+  authMiddleware,
+  redirectToSignIn,
+} from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // See https://clerk.com/docs/references/nextjs/auth-middleware
 // for more information about configuring your Middleware
 export default authMiddleware({
+  afterAuth(auth, req, evt) {
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    if (
+      auth.userId &&
+      auth.sessionClaims?.metadata.role !== "admin" &&
+      req.nextUrl.pathname !== "/"
+    ) {
+      const permission = new URL("/", req.url);
+      return NextResponse.redirect(permission);
+    }
+  },
   // Allow signed out users to access the specified routes:
-  publicRoutes: ["/", "/api/uploadthing"],
+  publicRoutes: ["/", "/menu", "/api/uploadthing"],
 });
 
 export const config = {
