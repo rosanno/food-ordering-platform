d4bb663ca85d3171f617fdfe602083b7e4051d5a
rosanno <rosannomacabuhay@gmail.com>
2024-04-17 09:28:36 +0800

    updated navbar hover & active class

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import Link from "next/link";
-import {
-  SignOutButton,
-  UserButton,
-  useUser,
-} from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+import { UserButton, useUser } from "@clerk/nextjs";
+
+import { cn } from "@/lib/utils";
 import ButtonLink from "./ui/button-link";
 
 const MenuList = [
@@ -28,6 +27,7 @@ const MenuList = [
 ];
 
 export default function Navbar() {
+  const pathname = usePathname();
   const { isSignedIn, user } = useUser();
 
   return (
@@ -37,40 +37,52 @@ export default function Navbar() {
         py-2.5 
         md:py-4
         md:px-5
+        fixed
+        top-0
+        inset-x-0
+        bg-white
+        shadow-sm
         w-full
-        max-w-6xl
-        mx-auto
       "
     >
-      <div className="flex items-center justify-between">
-        <Link
-          href={"/"}
-          className="font-extrabold md:text-lg"
-        >
-          Good<span className="text-[#FF9E0A]">Food</span>
-        </Link>
-        <nav className="hidden sm:block">
-          <ul className="flex items-center space-x-10">
-            {MenuList.map((list, i) => (
-              <li key={i} className="text-[13px]">
-                <Link href={list.path}>{list.label}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        {isSignedIn ? (
-          <div className="space-x-6">
-            {user?.publicMetadata.role ? (
-              <ButtonLink href="/admin/dashboard">
-                Dashboard
-              </ButtonLink>
-            ) : (
-              <UserButton />
-            )}
-          </div>
-        ) : (
-          <ButtonLink href="/sign-in">Sign in</ButtonLink>
-        )}
+      <div className="max-w-6xl mx-auto">
+        <div className="flex items-center justify-between">
+          <Link
+            href={"/"}
+            className="font-extrabold md:text-lg"
+          >
+            Good<span className="text-[#FF9E0A]">Food</span>
+          </Link>
+          <nav className="hidden sm:block">
+            <ul className="flex items-center space-x-10">
+              {MenuList.map((list, i) => (
+                <li
+                  key={i}
+                  className={cn(
+                    "text-[13px] hover:text-yellow-600 transition-colors duration-300",
+                    pathname === list.path &&
+                      "text-yellow-600"
+                  )}
+                >
+                  <Link href={list.path}>{list.label}</Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          {isSignedIn ? (
+            <div className="space-x-6">
+              {user?.publicMetadata.role ? (
+                <ButtonLink href="/admin/dashboard">
+                  Dashboard
+                </ButtonLink>
+              ) : (
+                <UserButton />
+              )}
+            </div>
+          ) : (
+            <ButtonLink href="/sign-in">Sign in</ButtonLink>
+          )}
+        </div>
       </div>
     </header>
   );
