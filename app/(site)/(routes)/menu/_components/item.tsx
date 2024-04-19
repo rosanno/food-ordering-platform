68a1f6db34272edbac4b4d26c4a60ba7d0b5cec8
rosanno <rosannomacabuhay@gmail.com>
2024-04-19 14:35:48 +0800

    update menu link

--- app/(site)/(routes)/menu/_components/item.tsx
+++ app/(site)/(routes)/menu/_components/item.tsx
@@ -14,13 +14,11 @@ interface ItemProps {
 
 const Item = ({ item }: ItemProps) => {
   return (
-    <Link href={`/details/${item.id}`}>
-      <div
-        className="
+    <div
+      className="
         bg-gray-100/30
         px-5
         py-2
-        lg:h-auto
         w-full
         rounded-lg 
         shadow-md 
@@ -28,7 +26,8 @@ const Item = ({ item }: ItemProps) => {
         duration-300
         mb-32
     "
-      >
+    >
+      <Link href={`/details/${item.id}`}>
         <div className="flex items-center justify-center">
           <Image
             src={item.imageUrl!!}
@@ -51,21 +50,21 @@ const Item = ({ item }: ItemProps) => {
             {formatCurrency(parseInt(item.price), "PHP")}
           </p>
         </div>
-        <div className="py-1.5 mt-2 flex items-center justify-between gap-2.5">
-          <Button
-            size={"sm"}
-            variant={"outline"}
-            className="w-full flex-1"
-            onClick={(e) => e.preventDefault()}
-          >
-            Order
-          </Button>
-          <Button size={"icon"} variant={"ghost"}>
-            <Heart className="h-5 w-5" />
-          </Button>
-        </div>
+      </Link>
+      <div className="py-1.5 mt-2 flex items-center justify-between gap-2.5">
+        <Button
+          size={"sm"}
+          variant={"outline"}
+          className="w-full flex-1"
+          onClick={(e) => e.preventDefault()}
+        >
+          Order
+        </Button>
+        <Button size={"icon"} variant={"ghost"}>
+          <Heart className="h-5 w-5" />
+        </Button>
       </div>
-    </Link>
+    </div>
   );
 };
 
