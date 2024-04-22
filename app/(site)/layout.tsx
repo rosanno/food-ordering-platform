63a94228d2912c1c219dea19e565b86719c2ca8c
rosanno <rosannomacabuhay@gmail.com>
2024-04-22 09:31:52 +0800

    fixed undefined restaurant find data

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -14,7 +14,7 @@ export default async function PublicLayout({
 
   const restaurant = await prisma.restaurant.findFirst({
     where: {
-      id: user?.id,
+      ownerId: user?.id,
     },
   });
 
