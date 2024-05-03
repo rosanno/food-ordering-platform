0c9f8e0e07106b9c237a666afb51394c205e5f45
rosanno <rosannomacabuhay@gmail.com>
2024-05-03 14:50:53 +0800

    created wish list page

---
+++ app/(site)/(routes)/favorites/_components/favorite-item.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { formatCurrency } from "@/lib/utils";
+import {
+  FavoriteItem as FavItem,
+  Menu,
+  Restaurant,
+} from "@prisma/client";
+import {
+  ShoppingCart,
+  StarIcon,
+  Trash2,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+interface FavoriteItemProps {
+  favoriteItem:
+    | (FavItem & {
+        menu: Menu & {
+          restaurant: Restaurant | null;
+        };
+      })
+    | null;
+}
+
+const FavoriteItem = ({
+  favoriteItem,
+}: FavoriteItemProps) => {
+  return (
+    <div className="flex bg-gray-100/50 p-2.5 relative">
+      <Link
+        href={`/menu/${favoriteItem?.menu.slug}`}
+        className="block w-full"
+      >
+        <div className="flex gap-3">
+          <div className="border border-gray-100/70 rounded-md p-3">
+            <Image
+              src={favoriteItem?.menu.imageUrl as string}
+              alt={favoriteItem?.menu.menuName as string}
+              height={130}
+              width={130}
+              className="h-16 w-16 object-contain"
+            />
+          </div>
+          <div className="space-y-1">
+            <h3 className="text-sm text-muted-foreground">
+              {favoriteItem?.menu.menuName}
+            </h3>
+            <h4 className="text-[12px] text-muted-foreground truncate">
+              Restaurant:{" "}
+              {favoriteItem?.menu.restaurant?.name}
+            </h4>
+            <div className="flex items-center gap-1">
+              {[1, 2, 3, 4, 5].map((_) => (
+                <StarIcon
+                  key={_}
+                  className="text-[#FFB500] h-3 w-3"
+                  fill="#FFB500"
+                />
+              ))}
+            </div>
+            <h4 className="pt-2 text-yellow-500 font-medium">
+              {formatCurrency(
+                parseInt(favoriteItem?.menu.price!!),
+                "PHP"
+              )}
+            </h4>
+          </div>
+        </div>
+      </Link>
+      <div className="ml-auto flex flex-col justify-between">
+        <Button variant={"ghost"} size={"icon"}>
+          <Trash2 className="h-4 w-4 text-red-500" />
+        </Button>
+        <Button variant={"outline"} size={"icon"}>
+          <ShoppingCart className="h-4 w-4" />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default FavoriteItem;
--- app/(site)/(routes)/favorites/page.tsx
+++ app/(site)/(routes)/favorites/page.tsx
@@ -1,23 +1,37 @@
 import { auth } from "@clerk/nextjs";
 import prisma from "@/lib/prisma";
 
+import FavoriteItem from "./_components/favorite-item";
+
 const FavoritePage = async () => {
   const { userId } = auth();
 
-  const favorites = await prisma.favorite.findMany({
+  const favorites = await prisma.favorite.findFirst({
     where: {
       customerId: userId as string,
     },
     include: {
       favoriteItem: {
         include: {
-          menu: true,
+          menu: {
+            include: {
+              restaurant: true,
+            },
+          },
         },
       },
     },
   });
 
-  return <div>Favorite Pages</div>;
+  return (
+    <section className="pt-11">
+      <div className="space-y-3.5">
+        {favorites?.favoriteItem.map((item) => (
+          <FavoriteItem key={item.id} favoriteItem={item} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default FavoritePage;
