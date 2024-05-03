0f8e68f5b096babc1f039765a711838f5cfab573
rosanno <rosannomacabuhay@gmail.com>
2024-05-03 12:12:35 +0800

    updated favorite route handler

--- app/api/favorites/route.ts
+++ app/api/favorites/route.ts
@@ -13,9 +13,56 @@ export async function POST(request: NextRequest) {
       });
     }
 
-    return NextResponse.json({
-      message: "Added to favorite",
+    const userFavorite = await prisma.favorite.findFirst({
+      where: {
+        customerId: userId,
+      },
+      include: { favoriteItem: true },
     });
+
+    if (!userFavorite) {
+      await prisma.favorite.create({
+        data: {
+          customerId: userId,
+          favoriteItem: {
+            create: {
+              menuId,
+            },
+          },
+        },
+      });
+
+      return NextResponse.json({
+        message: "Added to favorite",
+      });
+    }
+
+    const alreadyFavorite = userFavorite?.favoriteItem.find(
+      (item) => item.menuId === menuId
+    );
+
+    if (alreadyFavorite) {
+      await prisma.favoriteItem.delete({
+        where: {
+          id: alreadyFavorite.id,
+        },
+      });
+
+      return NextResponse.json({
+        message: "Removed from favorite",
+      });
+    } else {
+      await prisma.favoriteItem.create({
+        data: {
+          favoriteId: userFavorite.id,
+          menuId,
+        },
+      });
+
+      return NextResponse.json({
+        message: "Added to favorite",
+      });
+    }
 
   } catch (error) {
     console.log("[FAVORITES_POST]", error);
