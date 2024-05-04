78349309df85e10ffeec7b021af7d9a31bfcee81
rosanno <rosannomacabuhay@gmail.com>
2024-05-04 20:29:10 +0800

    added alert-dialog component

---
+++ components/modals/alert-modal.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+
+interface AlertModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  loading: boolean;
+}
+
+const AlertModal = ({
+  isOpen,
+  onClose,
+  onConfirm,
+  loading,
+}: AlertModalProps) => {
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  };
+
+  return (
+    <AlertDialog open={isOpen} onOpenChange={onChange}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <Button
+            variant={"outline"}
+            disabled={loading}
+            onClick={() => onClose()}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant={"destructive"}
+            disabled={loading}
+            onClick={onConfirm}
+          >
+            Continue
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default AlertModal;
