38edfb625dbf9d309986b3f6fc08d5c1e096e0c8
rosanno <rosannomacabuhay@gmail.com>
2024-05-05 19:50:25 +0800

    added ui loader

---
+++ app/(site)/(routes)/favorites/loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+  return (
+    <div className="h-96 flex items-center justify-center">
+      <div className="loader">
+        <span>goodfood</span>
+        <span>goodfood</span>
+      </div>
+    </div>
+  );
+}
