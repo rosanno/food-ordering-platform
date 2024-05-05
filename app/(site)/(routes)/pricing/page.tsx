acf750f02d7a55c49a6b9aa496ab24731d9bd25b
rosanno <rosannomacabuhay@gmail.com>
2024-05-05 20:10:41 +0800

    +

--- app/(site)/(routes)/pricing/page.tsx
+++ app/(site)/(routes)/pricing/page.tsx
@@ -1,5 +1,11 @@
 const PricingPage = () => {
-  return <section className="mt-28"></section>;
+  return (
+    <section className="mt-10">
+      <h1 className="text-2xl font-bold">
+        This page is under development
+      </h1>
+    </section>
+  );
 };
 
 export default PricingPage;
