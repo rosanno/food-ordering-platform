b202120560e649dcaf484a2d9c07d95a9fd1d11b
rosanno <rosannomacabuhay@gmail.com>
2024-05-18 11:18:01 +0800

    added filtering for restaurant order

--- app/admin/(routes)/[slug]/(routes)/orders/page.tsx
+++ app/admin/(routes)/[slug]/(routes)/orders/page.tsx
@@ -5,26 +5,43 @@ import Header from "@/components/ui/header";
 import { Order } from "./_components/column";
 import Client from "./_components/client";
 
-const OrdersPage = async () => {
+const OrdersPage = async ({
+  params,
+}: {
+  params: { slug: string };
+}) => {
   const orders = await prisma.orderItem.findMany({
     include: {
-      order: true,
+      order: {
+        include: {
+          restaurant: true,
+        },
+      },
       menu: true,
     },
   });
+
+  const foundItems = orders.filter(
+    (item) => item.order.restaurant.slug === params.slug
+  );
 
   // const totalAmount = orders.reduce((acc, item) => {
   //   return acc + parseInt(item.menu.price) * item.quantity;
   // }, 0);
 
-  const transformOrder: Order[] = orders.map((item) => ({
-    id: item.id,
-    customer: item.order.customer,
-    menu: item.menu.menuName,
-    image: item.menu.imageUrl!,
-    status: item.order.status,
-    createdAt: format(item.order.createdAt, "d MMMM, yyyy"),
-  }));
+  const transformOrder: Order[] = foundItems.map(
+    (item) => ({
+      id: item.id,
+      customer: item.order.customer,
+      menu: item.menu.menuName,
+      image: item.menu.imageUrl!,
+      status: item.order.status,
+      createdAt: format(
+        item.order.createdAt,
+        "d MMMM, yyyy"
+      ),
+    })
+  );
 
   return (
     <>
