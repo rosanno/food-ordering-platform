c2766dc9d122ea036af776ce331ab37054301f8c
rosanno <rosannomacabuhay@gmail.com>
2024-05-22 12:57:25 +0800

    updated category modal functionality

--- components/modals/category-modal.tsx
+++ components/modals/category-modal.tsx
@@ -2,6 +2,8 @@
 
 import * as z from "zod";
 import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useCategoryModal } from "@/hooks/use-category-modal";
@@ -13,28 +15,38 @@ import {
   FormField,
   FormItem,
   FormLabel,
-} from "../ui/form";
-import { Input } from "../ui/input";
-import { Button } from "../ui/button";
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
 
 const formSchema = z.object({
-  category: z.string().min(10),
+  name: z.string().min(10),
 });
 
 const CategoryModal = () => {
   const categoryModal = useCategoryModal();
+  const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      category: "",
+      name: "",
     },
   });
 
   const onSubmit = async (
     values: z.infer<typeof formSchema>
   ) => {
-    console.log(values);
+    try {
+      await axios.post("/api/blog-category", values);
+
+      toast.success("category created");
+      categoryModal.onClose;
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
   };
 
   return (
@@ -51,7 +63,7 @@ const CategoryModal = () => {
         >
           <FormField
             control={form.control}
-            name="category"
+            name="name"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Category</FormLabel>
