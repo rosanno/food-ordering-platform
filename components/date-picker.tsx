b819293dfc1905625ac3ca17149dde5250d28bf8
rosanno <rosannomacabuhay@gmail.com>
2024-05-24 12:55:16 +0800

    improve date-picker component functionality

--- components/date-picker.tsx
+++ components/date-picker.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import * as React from "react";
 import { format } from "date-fns";
 import { Calendar as CalendarIcon } from "lucide-react";
 import { ControllerRenderProps } from "react-hook-form";
@@ -20,24 +19,22 @@ interface DatePickerProps {
 }
 
 const DatePicker = ({ field }: DatePickerProps) => {
-  const [date, setDate] = React.useState<Date>();
-
   return (
     <Popover>
       <PopoverTrigger asChild>
         <Button
           variant={"outline"}
           className={cn(
-            "w-full justify-start text-left font-normal",
-            !date && "text-muted-foreground"
+            "w-full pl-3 text-left font-normal",
+            !field.value && "text-muted-foreground"
           )}
         >
-          <CalendarIcon className="mr-2 h-4 w-4" />
           {field.value ? (
             format(field.value, "PPP")
           ) : (
             <span>Pick a date</span>
           )}
+          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0">
@@ -46,7 +43,7 @@ const DatePicker = ({ field }: DatePickerProps) => {
           selected={field.value}
           onSelect={field.onChange}
           disabled={(date) =>
-            date > new Date() ||
+            date < new Date() ||
             date < new Date("1900-01-01")
           }
           initialFocus
